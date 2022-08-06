58171d6f7fb6e588e381a70b150e14fc54314fbf
Dillon Tan Zhao Kai <dillon.tanzhaokai@gmail.com>
2022-08-07 01:28:59 +0800

    Add recursive sum function template

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,13 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+    function sum(num1, num2) {
+        if (true) {
+            return
+        } else {
+            
+        }
+    }
 
+    return 
 };
 
 // Do not edit below this line
