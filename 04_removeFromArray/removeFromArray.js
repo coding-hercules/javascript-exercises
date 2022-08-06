03799232b6d44bfc983dbb57ee0df2022f9a0219
Dillon Tan Zhao Kai <dillon.tanzhaokai@gmail.com>
2022-08-07 00:43:05 +0800

    Add method to remove single array element from argument

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,11 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(arr, numRemove) {
+    
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] === numRemove) {
+            arr.splice(i, 1)
+        }
+        
+    }
 };
 
 // Do not edit below this line
