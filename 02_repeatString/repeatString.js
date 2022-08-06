b001e125f8fb1ee100b6f15ebfbfcdb2666c7414
Dillon Tan Zhao Kai <dillon.tanzhaokai@gmail.com>
2022-08-06 21:10:09 +0800

    Add loop in function (pre-test)

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,10 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    for (let i = 1; i < num + 1; i++) {
+        let text = '';
+        text += string;
+         
+    }
+    return text
 };
 
 // Do not edit below this line
