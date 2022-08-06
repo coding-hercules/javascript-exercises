470398d537cfd2edf860c42ce287c3b099f00fe6
Dillon Tan Zhao Kai <dillon.tanzhaokai@gmail.com>
2022-08-07 00:55:46 +0800

    Add return statement at end of function exp

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -3,9 +3,11 @@ const removeFromArray = function(arr, numRemove) {
     for (let i = 0; i < arr.length; i++) {
         if (arr[i] === numRemove) {
             arr.splice(i, 1)
+            continue
         }
-        
+        continue
     }
+    return arr 
 };
 
 // Do not edit below this line
