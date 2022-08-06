4d5ca6b851c05b2ae2b384147ca658a0c2635ff8
Dillon Tan Zhao Kai <dillon.tanzhaokai@gmail.com>
2022-08-06 22:47:52 +0800

    Remove unnecessary comment labels

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -2,7 +2,7 @@ const reverseString = function(string) {
     let reversedString = '';
     
     // split word/phrase into individual characters
-    let arr = string.split("") //this is an array
+    let arr = string.split("") 
     // put last character (index = length - 1) at index=0.
     for (let i = arr.length - 1; i >= 0; i--) {
         reversedString += arr[i];
