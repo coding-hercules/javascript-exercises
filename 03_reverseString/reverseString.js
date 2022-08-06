4f7b0aa2bb16944f11f0ef3d9549a2bdfe54438b
Dillon Tan Zhao Kai <dillon.tanzhaokai@gmail.com>
2022-08-06 22:34:13 +0800

    Add array-splitting and loop that combines arr elements from last element

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,15 @@
-const reverseString = function() {
+const reverseString = function(string) {
+    let reversedString = '';
+    
+    // split word/phrase into individual characters
+    let arr = string.split("") //this is an array
+    // put last character (index = length - 1) at index=0.
+    for (let i = arr.length - 1; i >= 0; i--) {
+        reversedString += arr[i];
+    }
+    //return final string
 
+    return reversedString
 };
 
 // Do not edit below this line
