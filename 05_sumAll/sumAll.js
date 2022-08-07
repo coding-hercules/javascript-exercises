f8f264cef85602d72c958b2450b0258c5d380deb
Dillon Tan Zhao Kai <dillon.tanzhaokai@gmail.com>
2022-08-07 10:33:11 +0800

    Add conditional that swaps start & end numbers if start>end
    I used destructuring assignment involving arrays to swap values.

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -10,7 +10,10 @@ const sumAll = function(num1, num2) {
     
     return sum(num1, num2) */
 
-    
+    if (num1 > num2) {
+        [num1, num2] = [num2, num1];
+    }
+
 };
 
 // Do not edit below this line
