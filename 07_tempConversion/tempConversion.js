4b03288cc0a142f4e2d324ae588821f8a3e21150
Dillon Tan Zhao Kai <dillon.tanzhaokai@gmail.com>
2022-08-07 15:44:59 +0800

    Add return calculation for ctof function

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,9 @@
-const ftoc = function() {
+const ftoc = function(fahrenheit) {
 
 };
 
-const ctof = function() {
-
+const ctof = function(celsius) {
+  return parseFloat((celsius * 9 / 5 + 32).toFixed(1))
 };
 
 // Do not edit below this line
