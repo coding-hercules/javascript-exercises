fec8ce76624679659a5aa17fe3abeaed13556b50
Dillon Tan Zhao Kai <dillon.tanzhaokai@gmail.com>
2022-08-07 11:06:44 +0800

    Add alternate solution using simple for-loop

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,5 @@
 const sumAll = function(num1, num2) {
-    if (typeof arguments[0] !== 'number' || typeof arguments[1] !== 'number') {
+    /*if (typeof arguments[0] !== 'number' || typeof arguments[1] !== 'number') {
         return 'ERROR'
     } else if (num1 < 0 || num2 < 0) {
         return 'ERROR'
@@ -17,9 +17,13 @@ const sumAll = function(num1, num2) {
             }
         }
     }
-    return sum(num1, num2)
+    return sum(num1, num2)*/
 
-    
+    let sum = 0;
+    for (let i = num1; i <= num2; i++) {
+        sum+=i
+    }
+    return sum
 
 };
 
