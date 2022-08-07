24dcfa9526cb23f4c7508318df3fa8c4bcdfa165
Dillon Tan Zhao Kai <dillon.tanzhaokai@gmail.com>
2022-08-07 10:57:38 +0800

    Rephrase condition checking for NaN arguments

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,20 +1,22 @@
 const sumAll = function(num1, num2) {
-    if (num1 < 0 || num2 < 0 || typeof arguments !== 'number') {
+    if (typeof num1 !== 'number' || typeof num2 !== 'number') {
         return 'ERROR'
-    }
+    } else if (num1 < 0 || num2 < 0) {
+        return 'ERROR'
+    } else {
 
-    if (num1 > num2) {
-        [num1, num2] = [num2, num1];
-    }
-    
-    function sum(startNum, endNum) {
-        if (startNum === endNum) {
-            return startNum
-        } else {
-            return sum(startNum, endNum - 1) + endNum
+        if (num1 > num2) {
+            [num1, num2] = [num2, num1];
+        }
+        
+        function sum(startNum, endNum) {
+            if (startNum === endNum) {
+                return startNum
+            } else {
+                return sum(startNum, endNum - 1) + endNum
+            }
         }
     }
-    
     return sum(num1, num2)
 
     
